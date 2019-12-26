e130e253d6dbe4947b6d6920623ac3ac45c9841c
Tsubasa Nakaoka <nkok.tsubasa0422@gmail.com>
2019-12-27 03:40:55 +0900

    A new file

---
+++ led.sh
@@ -0,0 +1,46 @@
+echo 1 > /dev/myled0
+sleep 0.33s
+echo 0 > /dev/myled0
+sleep 0.33s
+echo 1 > /dev/myled0
+sleep 0.33s
+echo 0 > /dev/myled0
+sleep 0.33s
+echo 1 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+sleep 1s
+
+echo 1 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+sleep 0.33s
+echo 1 > /dev/myled0
+sleep 0.33s
+echo 0 > /dev/myled0
+sleep 1s
+
+echo 1 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+sleep 0.33s
+echo 1 > /dev/myled0
+sleep 0.33s
+echo 0 > /dev/myled0
+sleep 0.33s
+echo 1 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+sleep 1s
+
+echo 1 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+sleep 0.33s
+echo 1 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+sleep 0.33s
+echo 1 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0

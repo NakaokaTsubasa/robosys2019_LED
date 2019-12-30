4d2286ed61a205d9a9b95dd66286cb44d52065c3
NakaokaTsubasa <57789113+NakaokaTsubasa@users.noreply.github.com>
2019-12-31 05:04:24 +0900

    Delete .myled.ko.cmd

--- .myled.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/robosys2019_LED/myled.ko := ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/pi/robosys2019_LED/myled.ko /home/pi/robosys2019_LED/myled.o /home/pi/robosys2019_LED/myled.mod.o ;  true
